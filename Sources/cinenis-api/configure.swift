e800529c18f88a93ad36a882750166bdda8415e5
mariana ortiz <moz80443@lasallebajio.edu.mx>
2025-06-03 17:13:44 -0600

    Conexión a base de datos, migrations y models

--- Sources/cinenis-api/configure.swift
+++ Sources/cinenis-api/configure.swift
@@ -9,11 +9,11 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
 app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST") ?? "db-mysql-sfo3-39177-do-user-22401106-0.l.db.ondigitalocean.com",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 25060,
-        username: Environment.get("DATABASE_USERNAME") ?? "doadmin",
-        password: Environment.get("DATABASE_PASSWORD") ?? "AVNS_Rxpp3bi_P88b2md3VVx",
-        database: Environment.get("DATABASE_NAME") ?? "cinenis_db",
+        hostname: Environment.get("DATABASE_HOST"),
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)),
+        username: Environment.get("DATABASE_USERNAME"),
+        password: Environment.get("DATABASE_PASSWORD") ,
+        database: Environment.get("DATABASE_NAME"),
         tlsConfiguration: .forClient(certificateVerification: .none)
     ), as: .mysql)
 
