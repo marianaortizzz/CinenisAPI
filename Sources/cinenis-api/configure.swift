57ab1b2981a35bf53b0fabbcb6cfaf236c56673d
mariana ortiz <moz80443@lasallebajio.edu.mx>
2025-06-04 23:34:30 -0600

    Cambio en la base de datos; modificacion del tipo de dato de las llaves primarias en todos los modelos de UUID a int autoincrementable (Cambios aplicado a las migrations y ya ejecutados en la base de datos)

--- Sources/cinenis-api/configure.swift
+++ Sources/cinenis-api/configure.swift
@@ -9,11 +9,11 @@ public func configure(_ app: Application) async throws {
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
 app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST"),
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)),
-        username: Environment.get("DATABASE_USERNAME"),
-        password: Environment.get("DATABASE_PASSWORD") ,
-        database: Environment.get("DATABASE_NAME"),
+        hostname: Environment.get("DATABASE_HOST") ?? "",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 25060,
+        username: Environment.get("DATABASE_USERNAME") ?? "",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "",
         tlsConfiguration: .forClient(certificateVerification: .none)
     ), as: .mysql)
 
