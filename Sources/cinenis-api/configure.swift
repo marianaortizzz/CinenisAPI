5b1691efb820432bfe0e806221f88186e8516b57
SergioErnestoRosasDucoing <rosasducoing@gmail.com>
2025-06-05 01:34:18 -0600

    Agrega función loadEnvFile para cargar variables de entorno desde .env

--- Sources/cinenis-api/configure.swift
+++ Sources/cinenis-api/configure.swift
@@ -3,17 +3,36 @@ import Fluent
 import FluentMySQLDriver
 import Vapor
 
+
+func loadEnvFile() {
+    let envPath = FileManager.default.currentDirectoryPath + "/.env"
+    guard let content = try? String(contentsOfFile: envPath) else { return }
+
+    content
+        .split(separator: "\n")
+        .map { $0.split(separator: "=", maxSplits: 1) }
+        .forEach { pair in
+            guard pair.count == 2 else { return }
+            let key = String(pair[0])
+            let value = String(pair[1])
+            setenv(key, value, 1)
+        }
+}
+
+
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    loadEnvFile()
+
 
 app.databases.use(DatabaseConfigurationFactory.mysql(
-        hostname: Environment.get("DATABASE_HOST"),
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)),
-        username: Environment.get("DATABASE_USERNAME"),
-        password: Environment.get("DATABASE_PASSWORD") ,
-        database: Environment.get("DATABASE_NAME"),
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 3306,
+        username: Environment.get("DATABASE_USERNAME") ?? "root",
+        password: Environment.get("DATABASE_PASSWORD") ?? "",
+        database: Environment.get("DATABASE_NAME") ?? "cinenis",
         tlsConfiguration: .forClient(certificateVerification: .none)
     ), as: .mysql)
 
