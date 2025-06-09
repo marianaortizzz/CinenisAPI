2a90bfe7aaf827fb4a2906b5c20e752ac50968c9
Ruth Manriquez <ruthmanriquez@192.168.1.14>
2025-06-09 10:07:04 -0600

    Interfaz de SWiftUI al proyecto

---
+++ Interfaz/CinenisApp/CinenisApp/CinenisAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinenisAppApp.swift
+//  CinenisApp
+//
+//  Created by Ruth Manriquez on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct CinenisAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
